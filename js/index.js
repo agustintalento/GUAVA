7e97a79f0c458c705a18d1259220c3706b46a316
agustalento <agust193@gmail.com>
2022-07-06 22:35:57 -0300

    modifico dom dinamicamente y nuevas funciones

--- js/index.js
+++ js/index.js
@@ -2,69 +2,7 @@
 //en esta primera instancia, la idea es crear un cálculo de descuentos por promoción 3x2 y
 //además setear envio gratis cuando el precio es mayor a $1000
 
-
-let cantidadArticulos = parseInt(prompt('Cuantos artículos va a llevar'));
-let articulos = [];
-let precioArticulos = [];
-let precioTotal = 0;
 let envioGratis = false;
-
-/* se le pide al usuario la cantidad de productos que va a comprar,
-con la cantidad ingresada. se pide el precio de cada uno para sumar a un array */
-
-function agregarArticulo() {
-    for (let i = 0; i < cantidadArticulos; i++) {
-        precioArticulos.push(parseInt(prompt('precio del articulo'))); 
-    }
-
-}
-
-agregarArticulo();
-
-/* si la cantidad de elementos del array es de 3 o más se ordenan por precio,
-para setear el mas barato como producto gratis (por la promo 3x2) (ver en la consola el orden),
-se ve si la cantidad de productos que lleva el cliente es multiplo de 3, y con el multiplo este se 
-setea la cantidad de productos gratis, siempre siendo estos los más baratos */
-
-function promocion(){
-    if (precioArticulos.length > 2) {
-        precioArticulos.sort(function(a, b) {
-            return a - b;
-        });
-        
-        for (let i= 0; i < Math.floor((precioArticulos.length)/3) ; i++) {
-            precioArticulos[i] = 0;
-        }    
-        console.log(precioArticulos);
-    }
-}
-
-promocion();
-
-/* con la siguiente funcion, se suma el precio de los productos del array, dandonos el precio total
-de los productos que va a comprar el cliente, y si el precio es mayor a $1000, 
-el envio se hace gratis. Se agrega el total al index.html */
-
-function precioFinal() {
-    for (let i= 0; i < precioArticulos.length; i++) {
-        precioTotal = precioTotal + precioArticulos[i];
-    }
-    if(precioTotal >= 1000) {
-        envioGratis = true;
-        document.getElementById('precioTotal').innerHTML = 
-        `el precio final es de $ ${precioTotal} y el envio es gratis`;
-        
-    }
-    else {
-        document.getElementById('precioTotal').innerHTML = 
-        `el precio final es de $ ${precioTotal}`;
-        
-    }
-
-    
-}
-
-precioFinal();
 
 /* con el siguiente constructor, la idea es importar las plantas, para mostrarlas en el index 
 en forma de cards. El index va a tener una sección de plantas recomendadas, donde se clickea una planta
@@ -85,7 +23,75 @@ const dracena = new planta('Dracena', 'Arbusto', '55', true,  1100, './img/produ
 const pilea = new planta('Pilea peperomioides', 'Herbácea', '12', true, 400, './img/productos/pilea.jpg');
 const crisantemo = new planta('Crisantemo', 'Herbácea', '30', true, 320,'./img/productos/crisantemo.jpg');
 
+let arrayPlantas = [dracena, pilea, crisantemo];
 
-console.log(dracena);
-console.log(pilea);
-console.log(crisantemo);
+arrayPlantas.forEach(planta => {document.getElementById('plantas').innerHTML +=  `
+
+    <div class="card align-items-center" style="width: 18rem;">
+        <img src="${planta.src}" class="card-img-top" alt="dracena">
+    <div class="card-body">
+        <p class="card-text plantaNombre">${planta.nombre}</p>
+        <p class="card-text plantaNombre">$ ${planta.precio}</p>
+
+        <input id="${planta.nombre}" type="number" value="0" min="0" max="10" />
+    </div>
+    </div>
+`
+    
+});
+
+
+
+/* calculo el precio final de la compra y la muestro en el html */
+function promocion(){
+
+    let precioArticulos =[];
+    arrayPlantas.forEach(planta => { 
+        for (let i = 0; i < parseInt(document.getElementById(planta.nombre).value); i++) {
+            precioArticulos.push(planta.precio);
+            
+        }
+    });
+
+    if (precioArticulos.length > 2) {
+        precioArticulos.sort(function(a, b) {
+            return a - b;
+        });
+        
+        for (let i= 0; i < Math.floor((precioArticulos.length)/3) ; i++) {
+            precioArticulos[i] = 0;
+        }    
+    }
+    return precioArticulos;
+}
+
+function calculoTotal() {
+    let precioArticulos = promocion();
+    let precioFinal = 0;
+    precioArticulos.forEach(precio => { precioFinal += precio;
+        
+    });
+
+    
+    if(precioFinal >= 1000) {
+        envioGratis = true;
+        document.getElementById('precioTotal').innerHTML = 
+        `el precio final es de $ ${precioFinal} y el envio es gratis`;
+        
+    }
+    else {
+        document.getElementById('precioTotal').innerHTML = 
+        `el precio final es de $ ${precioFinal}`;
+        
+    }
+
+ 
+}
+
+
+
+
+
+
+
+document.getElementById("botonCarrito").addEventListener("click", calculoTotal);
